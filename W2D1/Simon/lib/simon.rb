4caa18260ee717fea5bdfcb1cb3ffdb1b3af39eb
SHP <skylarprill@gmail.com>
2018-10-28 22:15:28 -0700

    Flesh-out #show_sequence to display colors in terminal, and #require_sequence to read user input

--- W2D1/Simon/lib/simon.rb
+++ W2D1/Simon/lib/simon.rb
@@ -30,18 +30,34 @@ class Simon
 
   def show_sequence
     add_random_color
+    puts "This is the sequence:"
+    @seq.each do |color|
+      puts color
+      sleep(1)
+      system("clear")
+      sleep(.5)
+    end
   end
 
   def require_sequence
+    puts "Did you catch that? Let's find out!"
+    @seq.each do |color|
+      answer = gets.chomp
+      if answer[0] != color[0]
+        @game_over = true
+        break
+      end
+    end
 
   end
 
   def add_random_color
-    seq << COLORS.sample
+    @seq << COLORS.sample
   end
 
   def round_success_message
     puts "Congratulations! You passed round #{@sequence_length}!"
+    puts "Unfortunately, it only gets harder from here."
   end
 
   def game_over_message
