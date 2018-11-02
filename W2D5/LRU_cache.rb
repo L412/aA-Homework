1522a93f425457c99389e90abbf522f21a5a5259
SHP <skylarprill@gmail.com>
2018-11-01 23:15:46 -0700

    Write #initialize, #count, #add(el), and #show for LRUCache class

--- W2D5/LRU_cache.rb
+++ W2D5/LRU_cache.rb
@@ -1,20 +1,30 @@
 class LRUCache
-    def initialize
+
+    def initialize(size)
+      @size = size
+      @cache = []
     end
 
     def count
-      # returns number of elements currently in cache
+      @cache.count
     end
 
+    #this will be an inefficient method
+    #.include? is O(n) because we have to go through the entire array
     def add(el)
-      # adds element to cache according to LRU principle
+      if @cache.include?(el)
+        @cache.delete(el)
+        @cache.push(el)
+      elsif @cache.count >= @size
+        @cache.shift
+        @cache.push(el)
+      else
+        @cache.push(el)
+      end
     end
 
     def show
-      # shows the items in the cache, with the LRU item first
+    p @cache
     end
-
-    private
-    # helper methods go here!
 
   end
