e26f4964c0851720ffe5c0d9b64e2f292065447e
SHP <skylarprill@gmail.com>
2018-11-22 23:59:37 -0800

    Write javascript exercises on functions

---
+++ W5D5/javascript_practice.js
@@ -0,0 +1,14 @@
+//Simple Timeout Function
+function window.setTimeout( function() {
+  alert("HAMMERTIME")
+}, 5000);
+
+//Timeout with Closure
+function hammerTime(time) {
+  window.setTimeout( function() {
+    alert(`${time} is HAMMER TIME!`);
+  })
+  ${time} is hammertime!
+}
+
+//Some tea? Some biscuits?
